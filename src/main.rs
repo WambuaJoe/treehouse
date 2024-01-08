93acf216f1d62e02f3451886dc59f980b48b944c
WambuaJoe <kaytujohn254@gmail.com>
2024-01-08 15:57:48 +0300

    initialize Treehouse game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter name:");
+    let username = user_input();
+    println!("Hello, {}", username);
+}
+
+fn user_input() -> String {
+    let mut name = String::new();
+
+    stdin()
+        .read_line(&mut name)
+        .expect("Failed to read");
+    name
+        .trim()
+        .to_lowercase();
+
+    let visitor_list = ["Wambua", "Mwangi", "Wandeto"];
+    let mut allow_in = false;
+
+    for visitor in &visitor_list {
+        if visitor == &name {
+            allow_in = true;
+        }
+    }
+
+    if allow_in {
+        println!("Welcome to the Tree {}!", name);
+    } else {
+        println!("Access denied");
+    }
 }
